c06af191dc57c491ca296d2aa0d37994290da4d3
anilozdem <anil.ozdem2134@gmail.com>
2024-12-13 11:42:32 +0300

    first commit

---
+++ src/test/java/tests/day13_testNGFramework/C02_TestNG_Framework.java
@@ -0,0 +1,13 @@
+package tests.day13_testNGFramework;
+
+public class C02_TestNG_Framework {
+
+    /*
+        TestNG POM framework ile
+        temel hedefimiz test class'inda surekli kullandigimiz
+        1- WebDriver objesi
+        2- Locate'ler
+        3- Test data'lari (kullanici adi, sifre ....)
+
+     */
+}
