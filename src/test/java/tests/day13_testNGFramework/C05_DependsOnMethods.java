f342cc934838f110b51346de6617247cb320d556
anilozdem <anil.ozdem2134@gmail.com>
2024-12-13 13:42:09 +0300

    day13 sonu

--- src/test/java/tests/day13_testNGFramework/C05_DependsOnMethods.java
+++ src/test/java/tests/day13_testNGFramework/C05_DependsOnMethods.java
@@ -15,6 +15,32 @@ public class C05_DependsOnMethods {
     // 2- phone icin arama yapip urun bulunabildigini test edin
     // 3- ilk urunu tiklayip, urun isminde case sensitive olmadan "phone" bulundugunu test edin
 
+    /*
+        dependsOnMethods = "anasayfaTesti"
+
+     1- siralama icin degil, method'lari birbirine baglamak icin kullanilir
+           eger anasayafa testi calisip PASSED olmazsa
+           phoneAramaTesti calistirmanin hicbir anlami olmayacaksa
+           dependsOnMethods = "anasayfaTesti" yazabiliriz
+
+     2- her test method'u bagimsiz olarak calistirilabilir
+        ancak dependsOnMethods ile calismasi baska class'in calismasina baglanan bir method
+        bagimsiz olarak calistirilmak istendiginde
+        ONCE bagli oldugu method'u calistirir,
+        O method calisip PASSED olursa, kendisi de calisirü
+
+        ANCAAAKKK bu sadece 2 method icin gecerlidir
+        eger 3 method bu ornekte oldugu gibi birbirine bagli ise
+        3.method'u bagimsiz calistirmak istedigimizde tum method'lar calismaz
+        No tests were found ==> calistirilacak Test bulunamadi der
+
+     3- Her ne kadar siralama icin kullanilamsa da
+        dependsOnMethods kullanilmis olan bir method'a sira geldiginde
+        once bagli oldugu method'un calismasini saglayacagi icin
+        otomatik olarak bir siralama da yapmis olur
+
+     */
+
     @Test
     public void anasayfaTesti() {
 
@@ -29,7 +55,7 @@ public class C05_DependsOnMethods {
 
     }
 
-    @Test
+    @Test(dependsOnMethods = "anasayfaTesti")
     public void phoneAramaTesti(){
         // 2- phone icin arama yapip
         WebElement aramaKutusu = Driver.getDriver().findElement(By.id("global-search"));
@@ -46,7 +72,8 @@ public class C05_DependsOnMethods {
 
 
     }
-    @Test(priority = 23)
+
+    @Test(dependsOnMethods = "phoneAramaTesti")
     public void ilkurunIsimTesti(){
         // 3- ilk urunu tiklayip,
         Driver.getDriver().findElement(By.xpath("(//*[@class='prod-img'])[1]"))
