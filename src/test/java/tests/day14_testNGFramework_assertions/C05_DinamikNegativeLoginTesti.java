fa76a2df131e31182bd8bc60b9777caa540692ca
anilozdem <anil.ozdem2134@gmail.com>
2024-12-16 15:15:04 +0300

    day14 sonu

--- src/test/java/tests/day14_testNGFramework_assertions/C05_DinamikNegativeLoginTesti.java
+++ src/test/java/tests/day14_testNGFramework_assertions/C05_DinamikNegativeLoginTesti.java
@@ -5,30 +5,31 @@ import org.testng.annotations.Test;
 import pages.TestotomasyonuPage;
 import utilities.ConfigReader;
 import utilities.Driver;
-import utilities.ReusableMethods;
 
-public class C05_DinamikNergativeLoginTesti {
-    static TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+public class C05_DinamikNegativeLoginTesti {
 
     //1- https://www.testotomasyonu.com/ anasayfasina gidin
     //2- account linkine basin
-    // 3- 3 farkli test method’u olusturun.
-    // - gecerli email, gecersiz password
-    // - gecersiz email, gecerli password
-    // - gecersiz email, gecersiz password.
+    // 3-  3 farkli test method’u olusturun.
+    //	- gecerli email, gecersiz password
+    //	- gecersiz email, gecerli password
+    //	- gecersiz email, gecersiz password.
     //4- Login butonuna basarak login olmayi deneyin
     //5- Basarili olarak giris yapilamadigini test edin
 
+    static TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+
     @Test
-    public void gecerliEmailTesti(){
+    public void gecersizPasswordTesti() {
         testotomasyonuPage = new TestotomasyonuPage();
+
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
         Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
-
         //2- account linkine basin
-        testotomasyonuPage.accountLinki.click();
-
-        // - gecerli email, gecersiz password
+        testotomasyonuPage.accountLinki
+                .click();
+        // 3-  3 farkli test method’u olusturun.
+        //	- gecerli email, gecersiz password
         testotomasyonuPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecerliEmail"));
         testotomasyonuPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecersizPassword"));
 
@@ -36,50 +37,48 @@ public class C05_DinamikNergativeLoginTesti {
         testotomasyonuPage.loginButonu.click();
 
         //5- Basarili olarak giris yapilamadigini test edin
-        Assert.assertTrue(testotomasyonuPage.loginButonu.isDisplayed());
+        Assert.assertTrue(testotomasyonuPage.emailKutusu.isDisplayed());
 
-
+        Driver.quitDriver();
     }
+
     @Test
-    public void gecersizEmailTesti(){
+    public void gecersizEmailTesti() {
         testotomasyonuPage = new TestotomasyonuPage();
+
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
         Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
-
         //2- account linkine basin
         testotomasyonuPage.accountLinki.click();
-
-        //3- gecersiz email, gecerli password
+        //3-  3 farkli test method’u olusturun.
+        //	- gecersiz email, gecerli password
         testotomasyonuPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecersizEmail"));
         testotomasyonuPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecerliPassword"));
-
         //4- Login butonuna basarak login olmayi deneyin
         testotomasyonuPage.loginButonu.click();
-
         //5- Basarili olarak giris yapilamadigini test edin
-        Assert.assertTrue(testotomasyonuPage.loginButonu.isDisplayed());
-
+        Assert.assertTrue(testotomasyonuPage.emailKutusu.isDisplayed());
+        Driver.quitDriver();
     }
+
     @Test
-    public void gecerliEmailvePasswordTesti(){
+    public void gecersizEmailGecersizPasswordTesti() {
         testotomasyonuPage = new TestotomasyonuPage();
+
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
         Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
-
         //2- account linkine basin
         testotomasyonuPage.accountLinki.click();
-
-        //3- gecersiz email, gecersiz password
+        //3-  3 farkli test method’u olusturun.
+        //	- gecersiz email, gecersiz password.
         testotomasyonuPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecersizEmail"));
         testotomasyonuPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecersizPassword"));
-
         //4- Login butonuna basarak login olmayi deneyin
         testotomasyonuPage.loginButonu.click();
-
         //5- Basarili olarak giris yapilamadigini test edin
-        Assert.assertTrue(testotomasyonuPage.loginButonu.isDisplayed());
-
+        Assert.assertTrue(testotomasyonuPage.emailKutusu.isDisplayed());
         Driver.quitDriver();
 
     }
+
 }
---
+++ src/test/java/tests/day15_TestNGAssertions/C01_CokluAssertion.java
@@ -0,0 +1,11 @@
+package tests.day15_TestNGAssertions;
+
+import org.testng.annotations.Test;
+
+public class C01_CokluAssertion {
+
+    @Test
+    public void ilkUrunIsimTesti(){
+
+    }
+}
