9f7f0d6bb3bf67652b21ed066c8555b27e232abf
anilozdem <anil.ozdem2134@gmail.com>
2024-12-18 14:23:51 +0300

    Ders16 sonu

--- src/test/java/tests/day16_htmlReports/C06_TopluNegativeLoginTesti.java
+++ src/test/java/tests/day16_htmlReports/C06_TopluNegativeLoginTesti.java
@@ -1,8 +1,14 @@
 package tests.day16_htmlReports;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class C06_TopluNegativeLoginTesti {
+
 
     //1- https://www.testotomasyonu.com/ anasayfasina gidin
     //2- account linkine basin
@@ -11,14 +17,40 @@ public class C06_TopluNegativeLoginTesti {
     //5- Basarili olarak giris yapilamadigini test edin
     //   anil@hotmail.com  13579
 
-    @Test
-    public void negatifLoginTesti(String verilenEmail, String verilenSifre){
+    @DataProvider
+    public static Object[][] emailPasswordProvider() {
+
+        String[][] emailPasswordArr = {
+                {"anil@hotmail.com", "13579"},
+                {"ceren@gmail.com", "24680"},
+                {"cansu@yahoo.com","asdfgh"}
+        };
+
+        return emailPasswordArr;
+    }
+
+
+    @Test(dataProvider = "emailPasswordProvider")
+    public void negatifLoginTesti(String verilenEmail , String verilenSifre){
 
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
 
         //2- account linkine basin
-        //3- asagidaki listede verilen email ve sifreleri yazin
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
+
+        //3- parametre olarak verilen email ve sifreleri yazin
+        testotomasyonuPage.emailKutusu.sendKeys(verilenEmail);
+        testotomasyonuPage.passwordKutusu.sendKeys(verilenSifre);
+
         //4- Login butonuna basarak login olmayi deneyin
+        testotomasyonuPage.loginButonu.click();
+
         //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testotomasyonuPage.emailKutusu.isDisplayed());
+
+        Driver.quitDriver();
+
     }
 }
