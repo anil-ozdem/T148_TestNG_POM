e9e97e218f8bda0605cbf0d80d2cbaa04c642533
anilozdem <anil.ozdem2134@gmail.com>
2024-12-18 13:25:27 +0300

    Ders16 sonu

---
+++ src/test/java/tests/day16_htmlReports/C06_TopluNegativeLoginTesti.java
@@ -0,0 +1,24 @@
+package tests.day16_htmlReports;
+
+import org.testng.annotations.Test;
+
+public class C06_TopluNegativeLoginTesti {
+
+    //1- https://www.testotomasyonu.com/ anasayfasina gidin
+    //2- account linkine basin
+    //3- asagidaki listede verilen email ve sifreleri yazin
+    //4- Login butonuna basarak login olmayi deneyin
+    //5- Basarili olarak giris yapilamadigini test edin
+    //   anil@hotmail.com  13579
+
+    @Test
+    public void negatifLoginTesti(String verilenEmail, String verilenSifre){
+
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+
+        //2- account linkine basin
+        //3- asagidaki listede verilen email ve sifreleri yazin
+        //4- Login butonuna basarak login olmayi deneyin
+        //5- Basarili olarak giris yapilamadigini test edin
+    }
+}
