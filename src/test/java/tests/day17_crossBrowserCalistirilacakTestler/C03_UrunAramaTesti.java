414b5bd713b5112f21b1f6923882c2a963153240
anilozdem <anil.ozdem2134@gmail.com>
2024-12-19 12:34:36 +0300

    TestNG Sonu

--- src/test/java/tests/day17_crossBrowserCalistirilacakTestler/C03_UrunAramaTesti.java
+++ src/test/java/tests/day17_crossBrowserCalistirilacakTestler/C03_UrunAramaTesti.java
@@ -1,4 +1,44 @@
 package tests.day17_crossBrowserCalistirilacakTestler;
 
-public class C03_UrunAramaTesti {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBaseCross;
+
+import java.time.Duration;
+
+public class C03_UrunAramaTesti extends TestBaseCross {
+
+    @Test
+    public void test01(){
+
+
+        driver.get("https://www.testotomasyonu.com");
+
+        // phone icin arama yapin
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys("phone" + Keys.ENTER);
+
+        // arama sonucunda urun bulunabildigini test edin
+
+        WebElement aramaSonucuElementi = driver.findElement(By.className("product-count-text"));
+
+        String unExpectedSonuc = "0 Products Found";
+        String actualSonuc = aramaSonucuElementi.getText();
+
+        Assert.assertNotEquals(actualSonuc,unExpectedSonuc);
+        /*
+            JUnit Assertions ile 2 kucuk fark var
+            1- Class ismi Assertions degil Assert
+            2- JUnit'de once expected, sonra actual yaziliyordu
+               TestNG'de once actual, sonra expected yaziliyor
+         */
+
+
+    }
+
 }
